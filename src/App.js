2b73d7153bf4baff649a6f58815b04bdc557d941
Syed Wassi Ul Haque <wassi244255@gmail.com>
2021-04-05 19:10:25 +0600

    updated to 1st phase SPA

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Header/Shop/Shop';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,11 +3,32 @@ import React from 'react';
 const Cart = (props) => {
     const cart = props.cart; // didn't understand/
     const totalPrice = cart.reduce((total,prd) => total + prd.price, 0);
+
+    let shipping = 0;
+    if (totalPrice > 35){
+        shipping = 0;
+    }
+    else if (totalPrice > 15){
+        shipping = 4.99;
+    }
+    else if (totalPrice > 0){
+        shipping = 12.99
+    }
+    const Tax = (totalPrice / 10).toFixed(2);
+    const grandTotal = totalPrice + shipping + Number(Tax).toFixed(2);
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
     return (
         <div>
             <h4>Order Summery</h4>
             <p>Items Ordered: {cart.length}</p> 
-            <p>Total Price:{totalPrice} </p>
+            <p>Product Price:{formatNumber(totalPrice)} </p>
+            <p>Shipping Cost: {shipping} </p>
+            <p>Tax + VAT :{Tax} </p>
+            <p>Total Price:{grandTotal} </p>
         </div>
     );
 };
